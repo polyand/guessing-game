aa78ac889741a933ab69a1b6f30f3e0c816c8d40
Andriy Polyakov <paspolyakov@gmail.com>
2022-08-13 00:40:19 +0300

    guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        return Math.round((this.end - (this.end - this.start) / 2));
     }
 
     lower() {
-
+        this.end = Math.round(this.end - (this.end - this.start) / 2);
     }
 
     greater() {
-
+        this.start = Math.round(this.end - (this.end - this.start) / 2);
     }
 }
 
